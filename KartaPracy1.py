048442cb326ab7b157b988bee81f39086ba72b13
1b_17_MW <114583342+TehnikInformejtik@users.noreply.github.com>
2022-10-05 12:12:20 +0000

    skonczona kartapracy1

--- KartaPracy1.py
+++ KartaPracy1.py
@@ -1,10 +1,39 @@
 # zad 1
 
-# a = int(input())
-# b = int(input())
-# print(a**2 + b**2)
+a = int(input())
+b = int(input())
+print(a**2 + b**2)
 
 # zad 2
 a = int(input())
 b = int(input())
 print((a+b)**2)
+
+# zad 3
+a = int(input())
+b = int(input())
+print((a-b)**3)
+
+# zad 4
+a = int(input())
+b = int(input())
+c = int(input())
+print(a*b*c)
+
+# zad 4 v.2
+a, b, c = map(int, input().split)
+print(a*b*c)
+
+# zad 5
+a = int(input())
+b = int(input())
+print(round((a+b)*0.4, 2))
+
+# zad 6
+a = int(input())
+print(a/1.23)
+
+# zad 7
+a = int(input())
+b = int(input())
+print(a % b)
