0c7576675303a25c60c20afca8aaae159fd68b5e
1b_17_MW <114583342+TehnikInformejtik@users.noreply.github.com>
2022-10-10 10:16:12 +0000

    zrobiono 2 zadania

---
+++ KartaPracy2.py
@@ -0,0 +1,16 @@
+# zad 1
+# n = int(input())
+
+# if n % 3 == 0:
+#    print("tak")
+# else:
+#    print("nie")
+
+# zad 2
+# n = int(input())
+# if n >= 100 and n < 1000 and n % 17 == 0:
+#   print("tak")
+# else:
+#     print("nie")
+
+# zad 3
