105f4e9b838dd1f1baead44b2afcc56d370274a1
1b_17_MW <114583342+TehnikInformejtik@users.noreply.github.com>
2022-10-17 09:56:52 +0000

    zrobiono elegancko kartkowke (nie na ocene)

---
+++ Temporary.py
@@ -0,0 +1,14 @@
+k = int(input('podaj liczbe calkowita: '))
+l = int(input('podaj liczbe calkowita: '))
+m = int(input('podaj liczbe calkowita: '))
+if k == l:
+ print("TAK")
+ print(k,l)
+elif k == m:
+ print("TAK")
+ print(k,m)
+elif l == m:
+ print("TAK")
+ print(l,m)
+else:
+ print("NIE")
