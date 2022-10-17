723695af3dcfc22147fb69fb1ed13be4384aa682
1b_17_MW <114583342+TehnikInformejtik@users.noreply.github.com>
2022-10-17 10:19:52 +0000

    zrobiono zadania z range

---
+++ KartaPracy3.py
@@ -0,0 +1,24 @@
+# PRE
+
+# print(list(range(2,9,2)))
+# pętla liczb dwucyfrowych od 10 do 21
+# pętla liczb dwucyfrowych nieparzystych od 15 do 31  
+# pętla liczb dwucyfrowych malejąco (step ujemny)
+# pętla liczb dwucyfrowych malejąco (step dodatni)
+# pętla liczb 3-cyfrowych podzielnych przez 20
+
+# for i in range(3, 10):
+#    print(i)
+
+
+# zadania
+
+print(list(range(10,22)))
+
+print(list(range(15, 32, 2)))
+
+print(list(range(99, 9, -1)))
+
+print(list(range(-99, -9, 1)))
+
+print(list(range(100, 1000, 20)))
