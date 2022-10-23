78321dcaa6ba5f2c4461a8ce2241568ff5393809
1b_17_MW <114583342+TehnikInformejtik@users.noreply.github.com>
2022-10-23 18:12:16 +0000

    Zrobiono co potrafię

---
+++ KartaPracy3b.py
@@ -0,0 +1,50 @@
+#zad 1
+# for i in range(1,31,1):
+#   print(i,end=" ")
+
+#zad 2
+# n = int(input())
+# for i in range(0,n+1):
+#    if i%2:
+#      print(i*i, end=" ")
+
+#zad 3
+# for i in range(1000,10000, 379):
+#     print(i, end=" ")
+
+# zad 4
+# for i in range (100,1000):
+#   if i&5==0 or i%6==0 or i%11==0:
+#     print (i, end=" ")
+    
+#zad 5
+# n = int(input())
+# suma=0
+# for i in range(1,n+1):
+#   liczba=int(input())
+#   suma+=liczba
+# print("suma =",suma)
+
+#zad 6
+
+
+#zad 7
+
+
+#zad 8
+# W0 = float(input("podaj kwote wejsciowa: "))
+# L = float(input("podaj okres inwestycji (w latach): ")) 
+# if L%0.5!=0:
+#   print("Podaj okres z dokładnością do połowy roku")
+# else:
+#   L = L*12
+#   while L > 0:
+#     W0 = (0.06/12+1)*W0
+#     L -= 1
+#   print(round(W0,2))
+
+#zad 9
+
+
+#zad 10
+
